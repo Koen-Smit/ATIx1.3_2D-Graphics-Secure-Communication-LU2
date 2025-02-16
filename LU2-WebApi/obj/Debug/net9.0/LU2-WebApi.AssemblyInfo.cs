107f818021b725e1528d494fdadd4c7d7656a86e
KerrieMeister96 <koensmit13@gmail.com>
2025-02-17 00:51:43 +0100

    git

--- LU2-WebApi/obj/Debug/net9.0/LU2-WebApi.AssemblyInfo.cs
+++ LU2-WebApi/obj/Debug/net9.0/LU2-WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LU2-WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f07e6bfa2c6a6a6e7d7490cd0a6adff4e04ab7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34c3b4ee8249a71005132db39ba1f27726f8280")]
 [assembly: System.Reflection.AssemblyProductAttribute("LU2-WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LU2-WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
